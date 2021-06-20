0445508884a90e4ef28967b5db909a00b2c38d11
hitesh-n <83066442+hitesh-n@users.noreply.github.com>
2021-06-20 17:09:22 +0530

    Add files via upload

---
+++ nodemcu.ino
@@ -0,0 +1,32 @@
+#include <ESP8266WiFi.h>
+
+WiFiClient client;
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("Connecting to wifi"); 
+  WiFi.begin(ssid (wifi_name), pwd (password) );
+  
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void loop()
+{
+  if(client.connect("api.pushingbox.com",80))   //   "184.106.153.149" or api.thingspeak.com
+  {  
+    client.print("GET /pushingbox?devid=" + xxxxx + "&id=Sheet1&name=hi HTTP/1.1\r\nHost: api.pushingbox.com\r\nConnection: close\r\n\r\n");
+  }
+  client.stop();
+
+  Serial.println("Waiting...");
+  delay(10000);
+}
