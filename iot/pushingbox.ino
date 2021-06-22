8e5564e2c5686c87b3f0cfdc2f0029a710fc832f
hitesh-n <83066442+hitesh-n@users.noreply.github.com>
2021-06-22 16:16:44 +0530

    Update pushingbox.ino

--- iot/pushingbox.ino
+++ iot/pushingbox.ino
@@ -7,7 +7,7 @@ void setup()
   Serial.begin(115200);
   delay(10);
   Serial.println("Connecting to wifi"); 
-  WiFi.begin("ACT102417274243", "Virtualact1@");
+  WiFi.begin("**your_ssid**", "**your_password**");
   
   while (WiFi.status() != WL_CONNECTED) 
   {
@@ -23,7 +23,7 @@ void loop()
 {
   if(client.connect("api.pushingbox.com",80))   //   "184.106.153.149" or api.thingspeak.com
   {  
-    client.print("GET /pushingbox?devid=v96A83C8D85CF8C3&id=Sheet1&name=hi HTTP/1.1\r\nHost: api.pushingbox.com\r\nConnection: close\r\n\r\n");
+    client.print("GET /pushingbox?devid=" + **your_device_id** + "&id=Sheet1&name=hi HTTP/1.1\r\nHost: api.pushingbox.com\r\nConnection: close\r\n\r\n");
   }
   client.stop();
 
